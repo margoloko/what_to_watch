f58f160a598daadcb22de6800ce7bcce899ba39b
1 <margoloko@mail.ru>
2023-03-16 18:57:04 +0700

    add form

--- opinions_app.py
+++ opinions_app.py
@@ -2,11 +2,14 @@ from datetime import datetime
 from random import randrange
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, URLField
+from wtforms.validators import DataRequired, Length, Optional
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'MYSECRETKEY'
 
 db = SQLAlchemy(app)
 
@@ -16,6 +19,23 @@ class Opinion(db.Model):
     text = db.Column(db.Text, unique=True, nullable=False)
     source = db.Column(db.String(256))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+class OpinionForm(FlaskForm):
+    title = StringField(
+        'Введите название фильма',
+        validators=[DataRequired(message='Обязательное поле'),
+                    Length(1, 128)]
+    )
+    text = TextAreaField(
+        'Напишите мнение',
+        validators=[DataRequired(message='Обязательное поле')]
+    )
+    source = URLField(
+        'Добавьте ссылку на подробный обзор фильма',
+        validators=[Length(1, 256), Optional()]
+    )
+    submit = SubmitField('Добавить')
+
 
 @app.route('/')
 def index_view():
@@ -32,7 +52,7 @@ def add_opinion_view():
 
 @app.route('/opinions/<int:id>')
 def opinion_view(id):
-    opinion = Opinion.query.get(id)
+    opinion = Opinion.query.get_or_404(id)
     return render_template('opinion.html', opinion=opinion)
 
 if __name__ == '__main__':
