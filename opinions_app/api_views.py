ea8b7c7fce2088e121adf922e19e3ecec5397d18
1 <margoloko@mail.ru>
2023-03-19 15:15:17 +0700

    add CURD

--- opinions_app/api_views.py
+++ opinions_app/api_views.py
@@ -1,6 +1,8 @@
-from flask import jsonify
+# what_to_watch/opinions_app/api_views.py
 
-from . import app
+from flask import jsonify, request
+
+from . import app, db
 from .models import Opinion
 
 
@@ -10,3 +12,25 @@ def get_opinion(id):
     return jsonify({'opinion': opinion.to_dict()}), 200
 
 
+@app.route('/api/opinions/<int:id>/', methods=['PATCH'])
+def update_opinion(id):
+    data = request.get_json()
+    opinion = Opinion.query.get_or_404(id)
+    # Если метод get_or_404 не найдёт указанный ключ,
+    # то он выбросит исключение 404
+    opinion.title = data.get('title', opinion.title)
+    opinion.text = data.get('text', opinion.text)
+    opinion.source = data.get('source', opinion.source)
+    opinion.added_by = data.get('added_by', opinion.added_by)
+    # Все изменения нужно сохранить в базе данных
+    db.session.commit()
+    # При создании или изменении объекта вернём сам объект и код 201
+    return jsonify({'opinion': opinion.to_dict()}), 201
+
+
+@app.route('/api/opinions/<int:id>/', methods=['DELETE'])
+def delete_opinin(id):
+    opinion = Opinion.query.get_or_404(id)
+    db.session.delete(opinion)
+    db.session.commit()
+    return '', 204
