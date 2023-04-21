fc75d810ac766f83f61f5204dde48ae1237e3d43
Balakhonova Marina <90852680+margoloko@users.noreply.github.com>
2023-04-21 10:11:49 +0300

    Update api_views.py
    docstring

--- opinions_app/api_views.py
+++ opinions_app/api_views.py
@@ -12,6 +12,14 @@ from .views import random_opinion
 
 @app.route('/api/opinions/<int:id>/', methods=['GET'])
 def get_opinion(id):
+    """
+    Получает мнение по id.
+
+    :param id: id мнения
+    :type id: int    
+    :return: JSON-объект с мнением и код 200
+    :rtype: tuple
+    """
     opinion = Opinion.query.get(id)
     if opinion is None:
         raise InvalidAPIUsage('Мнение с указанным id не найдено', 404)
@@ -20,6 +28,14 @@ def get_opinion(id):
 
 @app.route('/api/opinions/<int:id>/', methods=['PATCH'])
 def update_opinion(id):
+    """
+    Обновляет мнение по id.
+
+    :param id: id мнения
+    :type id: int    
+    :return: JSON-объект с обновлённым мнением и код 201
+    :rtype: tuple
+    """
     data = request.get_json()
     if (
         'text' in data and
@@ -28,20 +44,24 @@ def update_opinion(id):
     opinion = Opinion.query.get(id)
     if opinion is None:
         raise InvalidAPIUsage('Мнение с указанным id не найдено', 404)
-    # Если метод get_or_404 не найдёт указанный ключ,
-    # то он выбросит исключение 404
     opinion.title = data.get('title', opinion.title)
     opinion.text = data.get('text', opinion.text)
     opinion.source = data.get('source', opinion.source)
     opinion.added_by = data.get('added_by', opinion.added_by)
-    # Все изменения нужно сохранить в базе данных
     db.session.commit()
-    # При создании или изменении объекта вернём сам объект и код 201
     return jsonify({'opinion': opinion.to_dict()}), 201
 
 
 @app.route('/api/opinions/<int:id>/', methods=['DELETE'])
 def delete_opinin(id):
+    """
+    Удаляет мнение по id.
+
+    :param id: id мнения
+    :type id: int
+    :return: пустой JSON-объект и код 204
+    :rtype: tuple
+    """
     opinion = Opinion.query.get(id)
     if opinion is None:
         raise InvalidAPIUsage('Мнение с указанным id не найдено', 404)
@@ -52,12 +72,24 @@ def delete_opinin(id):
 
 @app.route('/api/opinions/', methods=['GET'])
 def get_opinions():
+    """
+    Получает все мнения.
+
+    :return: JSON-объект со списком мнений и код 200
+    :rtype: tuple
+    """
     opinions = Opinion.query.all()
     opinions_list = [opinion.to_dict() for opinion in opinions]
     return jsonify({'opinions': opinions_list}), 200
 
 @app.route('/api/opinions/', methods=['POST'])
 def add_opinion():
+    """
+    Добавляет новое мнение о фильме.
+
+    :return: JSON-объект с добавленным мнением и код 201
+    :rtype: tuple
+    """
     data = request.get_json()
     if 'title' not in data or 'text' not in data:
         raise InvalidAPIUsage('В запросе отсутствуют обязательные поля')
@@ -71,6 +103,12 @@ def add_opinion():
 
 @app.route('/api/get-random-opinion/', methods=['GET'])
 def get_random_opinion():
+    """
+    Получает случайное мнение.
+
+    :return: JSON-объект со случайным мнением и код 200
+    :rtype: tuple
+    """
     opinion = random_opinion()
     if opinion is not None:
         return jsonify({'opinion': opinion.to_dict()}), 200
